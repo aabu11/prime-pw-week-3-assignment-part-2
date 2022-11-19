95ca6726d4d876c6b5cfc0b76d3a6a66f37fa0f0
Ahmed Abu <ahmedabu13@gmail.com>
2022-11-18 19:51:58 -0600

    resubmitted

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,14 +14,14 @@ for (let i = 0; i < 4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 //  HAD TO ADD IN LOOP
 console.log("count from 0 to 5");
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 6; i++) {
   console.log( i);
 }
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log("count from 3 to 5");
-for (let i = 3; i < 5; i++) {
+for (let i = 3; i < 6; i++) {
   console.log( i);
 }
 
@@ -37,8 +37,8 @@ for (let i = 2; i <= 10; i++)
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log("STRETCH: countdown from 5 to 0");
 
-for (let i = 5; i > 0; i--) {
-  console.log("Loop" + i);
+for (let i = 5; i > -1; i--) {
+  console.log(i);
 }
 
 // 2. For of loops
@@ -48,10 +48,9 @@ let stars = ["Polaris", "Gacrux", "Formalhaut", "Rigel", "Deneb"];
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log("Some stars:");
 
-for (let i = 0; i < stars.length; i++) {
-  console.log(stars[i]);
+for (let star of stars){
+  console.log(star)
 }
-
 // 3. While loops
 console.log("---- 3. While loop ----");
 
@@ -81,7 +80,7 @@ console.log("count backwards from 10 to 5");
 
 
 let countbackwards = 10;{
-  while (countbackwards > 4 || 4=== "Countdown"){
+  while (countbackwards > 4){
       console.log(countbackwards);
       countbackwards = countbackwards - 1;
   }
